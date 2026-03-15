3a3b00dadbfa850ed7a8e8805b4df0aa8c0e5b95
crow-kuma <zoologico.kc@gmail.com>
2026-03-15 20:19:04 +0900

    refactor: externalize Sanity client configuration to environment variables and disable CDN.

--- portfolioAstro/src/lib/sanity.js
+++ portfolioAstro/src/lib/sanity.js
@@ -1,8 +1,11 @@
 import { createClient } from "@sanity/client";
 
+const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
+const dataset = import.meta.env.PUBLIC_SANITY_DATASET;
+
 export const sanity = createClient({
-  projectId: "68vbgg4r",
-  dataset: "production",
-  useCdn: true,
+  projectId,
+  dataset,
+  useCdn: false,
   apiVersion: "2023-07-01",
 });
